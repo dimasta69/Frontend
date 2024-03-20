59e64bf7ba006b67eff82396d9294d62acadf049
Долгополов Дмитрий <DMSTVKO@gmail.com>
2024-03-20 23:12:28 +0300

    dsaddadasd

---
+++ src/Contents/PhotoOne/Message/Message.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './Message.css';
+
+
+
+const Message = () => {
+    return (
+        <div className="messages">
+            <textarea type='text' name="messages" />
+            <button />
+        </div>
+    )
+}
+
+export default Message;
--- src/Contents/PhotoOne/PhotoOne.jsx
+++ src/Contents/PhotoOne/PhotoOne.jsx
@@ -5,15 +5,18 @@ import catImage from "./../../cat.jpg";
 import Box from "./Box/Box";
 import Comments from "./Comments/Comments";
 import "./Comments/Comments.css"
-import Pagination from "../../Pagination/Pagination";
+import Pagination from "../../Pagination/Pagination"
+import Message from "./Message/Message";
 
 const PhotoOne = () => {
     return (
         <div className="photo_one">
             <img src={catImage}  style={{ width: '480px', height: '270px' }} />
             <Box />
+            <button className="buttons"/>
             <Comments />
             <Pagination />
+            <Message  />
         </div>
     )
 }
