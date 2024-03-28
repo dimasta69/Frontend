ff9f7b2cadf2f676c154116aff18539071eecff8
Dimasta <DMSTVKO@gmail.com>
2024-03-28 18:27:00 +0300

    lll

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import axios from 'axios';
 import "./Content.css"
 
 import Photos from "../Photos/Photos.jsx"
 import Head from "../Head/Head.jsx"
 import Pagination from "../Pagination/Pagination.jsx";
+
+axios.get('http://127.0.0.1:8000/core_api/photos/') 
+  .then(response => { 
+    console.log(JSON.stringify(response.data, null, 2));  
+  }) 
+  .catch(error => { 
+    console.error(error); 
+  });
 
 
 const Content = () => {
